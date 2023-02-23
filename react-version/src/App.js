b6a7d048a6c09130cb1995286990b93098ac3cae
JA <jalcornaoz@gmail.com>
2023-02-23 14:37:16 +0100

    primeras pruebas

---
+++ react-version/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Aviso from "./Aviso.js";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Parrafo de prueba</p>
+      <Aviso />
+    </div>
+  );
+}
+
+export default App;
